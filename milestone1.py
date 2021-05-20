35a56dc15ec7f55439a19332526a20d9278515fc
effiew52 <84222693+effiew52@users.noreply.github.com>
2021-05-20 16:52:47 +0800

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -3,6 +3,8 @@ import json
 import re
 from bs4 import BeautifulSoup
 from nltk.stem import PorterStemmer
+from bs4.element import Comment
+
 
 
 def tokenizer(content):
@@ -12,34 +14,46 @@ def indexing(docid,content,inverted_index):
 
     soup = BeautifulSoup(content,"html.parser")
     texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    text1 = " ".join(visible_texts)
     ps = PorterStemmer()
     position = 0
 
-    for text in texts:
-        for i in tokenizer(text):
-            token = ps.stem(i)
-            if token not in inverted_index.keys():
-                inverted_index[token] = {'freq': 1, docid: {'positions': [position]}}
-            # if token was in invertedIndex, update it
+
+    for i in tokenizer(text1):
+        token = ps.stem(i)
+        if token not in inverted_index.keys():
+            inverted_index[token] = {'freq': 1, docid: {'positions': [position]}}
+        # if token was in invertedIndex, update it
+        else:
+            inverted_index[token]['freq'] += 1
+            if docid not in inverted_index[token].keys():
+                inverted_index[token][docid] = {'positions': [position]}
             else:
-                inverted_index[token]['freq'] += 1
-                if docid not in inverted_index[token].keys():
-                    inverted_index[token][docid] = {'positions': [position]}
-                else:
-                    inverted_index[token][docid]['positions'].append(position)
-            position += 1
+                inverted_index[token][docid]['positions'].append(position)
+        position += 1
 
 
 
 def geturl_dic(folder):
     docid_url = {}  #  key: url,value: unique id num of doc
     inverted_index = {}  # inverted_index = {key: token; value: {'freq': INT, docid: {rank: INT, positions:[INT]}})
+
+    #count th total number of files
+    count1 = 0
+    for (root, dirs, files) in os.walk(folder):
+        count1 += len(files)
+        stop_pos = count1 // 3
+
+    count2 = 0
+    num_pindex = 1
     for (root, dirs, files) in os.walk(folder):
         for page_name in files:
+            count2 += 1
             file_loc = os.path.join(root, page_name)
-            print(file_loc)
             try:
-                f = open(file_loc, 'r', encoding='utf-8')
+
+                f = open(file_loc, 'rb')
                 file_dic = json.loads(f.read())
                 url = file_dic["url"]
                 content = file_dic['content']
@@ -49,12 +63,34 @@ def geturl_dic(folder):
                     docid_url[url] = docid
                 else:
                     docid = docid_url[url]
+
+                #store index into three
+                if num_pindex == 3:
+                    continue
+                elif count2 > stop_pos:
+                    jsonfile(inverted_index,num_pindex)
+                    num_pindex += 1
+                    count2 = 0
+                    inverted_index = {}
+
                 indexing(docid,content,inverted_index)
             except ValueError as e:
                 print(e)
-    print(inverted_index)
 
+    jsonfile(inverted_index,num_pindex)
 
+def jsonfile(invert_index, num_pindex):
+    with open("partialIndex"+str(num_pindex)+".json", "w")as f:
+        line = json.dumps(invert_index)
+        f.write(line)
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
 if __name__ == '__main__':
     geturl_dic("/Users/mac/Desktop/Test")
+
